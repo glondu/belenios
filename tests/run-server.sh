71b7db43dcfc05444fab1e37ce4e76235c7ca658
Stephane Glondu <steph@glondu.net>
2012-12-07 15:52:00 +0100

    Handle /static in the test server

--- tests/run-server.sh
+++ tests/run-server.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! -d _build ]; then
+  echo "This script should be run from the root of the source tree!"
+  exit 1
+fi
+
 RUNDIR=${SELENIOS_RUNDIR:-_run}
 
 mkdir -p \
@@ -8,11 +13,9 @@ mkdir -p \
   $RUNDIR/lib \
   $RUNDIR/run
 
-sed "s@_RUNDIR_@$RUNDIR@g" tests/ocsigenserver.conf.in > $RUNDIR/etc/ocsigenserver.conf
-
-if [ ! -d _build ]; then
-  echo "This script should be run from the root of the source tree!"
-  exit 1
-fi
+sed \
+  -e "s@_RUNDIR_@$RUNDIR@g" \
+  -e "s@_SRCDIR_@$PWD@g" \
+  tests/ocsigenserver.conf.in > $RUNDIR/etc/ocsigenserver.conf
 
 ocsigenserver -c $RUNDIR/etc/ocsigenserver.conf "$@"
