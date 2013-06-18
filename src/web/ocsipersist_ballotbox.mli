e27a377e8bdfe070bbcdef6a9c8aae12bc2d1a17
Stephane Glondu <steph@glondu.net>
2013-06-18 17:48:46 +0200

    Ocsipersist_ballotbox: check ballots

--- src/web/ocsipersist_ballotbox.mli
+++ src/web/ocsipersist_ballotbox.mli
@@ -1,7 +1,6 @@
 (** Ocsipersist-based ballot box *)
 
-module MakeBallotBox (G : Signatures.GROUP)
-  (U : sig val uuid : Uuidm.t end) : sig
+module MakeLwtRandom (G : Signatures.GROUP) : sig
 
   (** {2 Monadic definitions} *)
 
@@ -12,11 +11,22 @@ module MakeBallotBox (G : Signatures.GROUP)
   val random : Z.t -> Z.t t
   (** [random q] returns a random number modulo [q]. It uses a secure
       random number generator initialized by a 128-bit seed. *)
+end
+(** Lwt-compatible random number generation. *)
+
+exception Serialization of exn
+exception ProofCheck
+
+module type LWT_ELECTION = Signatures.ELECTION
+  with type elt = Z.t
+  and type 'a m = 'a Lwt.t
+
+module MakeBallotBox (E : LWT_ELECTION) : sig
 
   (** {2 Ballot box management} *)
 
   include Signatures.BALLOT_BOX
-  with type 'a m := 'a t
+  with type 'a m := 'a Lwt.t
   and type ballot = string
   and type record = string * Serializable_builtin_t.datetime
 end
