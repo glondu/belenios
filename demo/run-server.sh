24b0859fd4dbe93e953723475980f36cc516de16
Stephane Glondu <steph@glondu.net>
2014-01-07 14:39:41 +0100

    Parametrize sample configuration file over "ocamlc -where"

--- demo/run-server.sh
+++ demo/run-server.sh
@@ -7,6 +7,7 @@ fi
 
 BELENIOS_RUNDIR=${BELENIOS_RUNDIR:-_run}
 BELENIOS_TMPDIR=${BELENIOS_TMPDIR:-/tmp/belenios}
+OCAML_STDLIBDIR=$(ocamlc -where)
 
 mkdir -p \
   $BELENIOS_RUNDIR/etc \
@@ -16,6 +17,7 @@ mkdir -p \
   $BELENIOS_TMPDIR/run
 
 sed \
+  -e "s@_OCAML_STDLIBDIR_@$OCAML_STDLIBDIR@g" \
   -e "s@_TMPDIR_@$BELENIOS_TMPDIR@g" \
   -e "s@_RUNDIR_@$BELENIOS_RUNDIR@g" \
   -e "s@_SRCDIR_@$PWD@g" \
