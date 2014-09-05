c0900856729d05f34013b328ae941d7cf27e9db4
Stephane Glondu <steph@glondu.net>
2014-09-05 11:44:57 +0200

    Set compile-command to "make all" instead of "make check"

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((nil . ((eval . (setq compile-command (format "cd %s && make check" (locate-dominating-file buffer-file-name ".dir-locals.el")))))))
+((nil . ((eval . (setq compile-command (format "cd %s && make all" (locate-dominating-file buffer-file-name ".dir-locals.el")))))))
