84d0cb8da7ddf27bf728a302b57ca2e3b157625d
Stephane Glondu <steph@glondu.net>
2015-10-15 17:24:56 +0200

    Remove unused Helios's booth files

--- ext/booth/js/jscrypto/sha2.js
+++
@@ -1,7 +0,0 @@
-function hex_sha256(s) {
-    return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(s));
-}
-
-function b64_sha256(s) {
-    return sjcl.codec.base64.fromBits(sjcl.hash.sha256.hash(s), true);
-}
