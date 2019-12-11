f35f2a0c71c11841d22cdb182f91478025bd1d43
Stephane Glondu <steph@glondu.net>
2019-12-11 16:43:32 +0100

    Force hasNativeBigInt and really implement modPow and modInverse

--- ext/booth/js/jsbn/BigIntCompat.js
+++ ext/booth/js/jsbn/BigIntCompat.js
@@ -1225,13 +1225,37 @@
     // Copyright © 2019 Inria
     // See "LICENSE" for details
 
-    var hasNativeBigInt = false;
+    var hasNativeBigInt = true;
 
     if (hasNativeBigInt) {
         function toJSBN(a) { return new BigInteger(a.toString()); }
         function ofJSBN(a) { return BigInt(a.toString()); }
         var ZERO = BigInt(0);
         var ONE = BigInt(1);
+        var windowsize = 6;
+        var windowmask = (1 << windowsize) - 1;
+        var windowmaskBI = BigInt(windowmask);
+        function bitLength(n) {
+            var r = 0;
+            while (n > ZERO) {
+                r++;
+                n = n >> ONE;
+            }
+            return r;
+        }
+        function extEuclide(a, b) {
+            var r0 = a, r1 = b;
+            var s0 = ONE, s1 = ZERO;
+            var t0 = s1, t1 = s0;
+            var r, q, s, t;
+            while (r = r0 % r1) {
+                q = (r0 - r) / r1;
+                r0 = r1; r1 = r;
+                s = s0 - s1 * q; s0 = s1; s1 = s;
+                t = t0 - t1 * q; t0 = t1; t1 = t;
+            }
+            return [r1, s1, t1];
+        }
         this.BigIntCompat = {
             ZERO: ZERO,
             ONE: ONE,
@@ -1250,23 +1274,37 @@
                 if (a > b) return 1;
                 return 0;
             },
-            modPow: function(a, e, m) {
-                return ofJSBN(toJSBN(a).modPow(toJSBN(e), toJSBN(m)));
+            modPow: function(a, b, p) {
+                var T = new Array();
+                T[0] = ONE;
+                T[1] = a;
+                for (var i = 2; i < windowmask; i += 2) {
+                    var z = T[i / 2];
+                    T[i] = (z * z) % p;
+                    T[i+1] = (T[i] * a) % p;
+                }
+                var S = T[0];
+                for (var i = Math.ceil(bitLength(b) / windowsize) - 1; i >= 0; i--) {
+                    var z = (b >> BigInt(i * windowsize)) & windowmaskBI;
+                    S = (S * T[Number(z)]) % p;
+                    if (i) {
+                        for (var j = 0; j < windowsize; j++) {
+                            S = (S * S) % p;
+                        }
+                    }
+                }
+                return S;
             },
             modInverse: function(a, m) {
-                return ofJSBN(toJSBN(a).modInverse(toJSBN(m)));
+                var b = extEuclide(a, m);
+                if (b[0] != ONE) throw "modInverse called on not-coprime numbers";
+                var r = b[1] % m; if (r < ZERO) r += m;
+                return r;
             },
             isProbablePrime: function(n, p) {
                 return toJSBN(n).isProbablePrime(p);
             },
-            bitLength: function(n) {
-                var r = 0;
-                while (n > ZERO) {
-                    r++;
-                    n = n >> ONE;
-                }
-                return r;
-            },
+            bitLength: bitLength,
             shiftLeft: function(a, b) { return a << BigInt(b); },
             shiftRight: function(a, b) { return a >> BigInt(b); },
             and: function(a, b) { return a & b; }
