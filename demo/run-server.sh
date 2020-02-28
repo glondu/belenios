81d001af2254c89ebae0e9898e17721fe11e1aee
Stephane Glondu <steph@glondu.net>
2020-02-28 14:29:52 +0100

    No need to add OCaml stdlib to ocsigenserver configuration

--- demo/run-server.sh
+++ demo/run-server.sh
@@ -7,7 +7,6 @@ fi
 
 BELENIOS_RUNDIR=${BELENIOS_RUNDIR:-_run}
 BELENIOS_TMPDIR=${BELENIOS_TMPDIR:-/tmp/belenios}
-OCAML_STDLIBDIR=$(ocamlc -where)
 
 mkdir -p \
   $BELENIOS_RUNDIR/etc \
@@ -20,7 +19,6 @@ mkdir -p \
 touch $BELENIOS_RUNDIR/password_db.csv
 
 sed \
-  -e "s@_OCAML_STDLIBDIR_@$OCAML_STDLIBDIR@g" \
   -e "s@_TMPDIR_@$BELENIOS_TMPDIR@g" \
   -e "s@_RUNDIR_@$BELENIOS_RUNDIR@g" \
   -e "s@_SRCDIR_@$PWD@g" \
