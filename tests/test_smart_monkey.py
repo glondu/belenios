617112db15652e8e2337008a42eed042757ac835
Swergas <swergas@yahoo.fr>
2020-04-28 16:44:21 +0200

    refactoring: split different monkey tests and state machine states into several files

--- tests/test_smart_monkey.py
+++ tests/test_smart_monkey.py
@@ -4,26 +4,17 @@ import unittest
 import random
 import os
 import sys
+import time
 from distutils.util import strtobool
 
 from util.fake_sent_emails_manager import FakeSentEmailsManager
-from util.election_testing import remove_database_folder, remove_election_from_database, initialize_server, wait_a_bit, populate_credential_and_password_for_voters_from_sent_emails, populate_random_votes_for_voters
-from util.page_objects import ElectionHomePage, NormalVoteStep1Page, NormalVoteStep2Page, NormalVoteStep3Page, VoterLoginPage, NormalVoteStep5Page, NormalVoteStep6Page, BallotBoxPage
-from util.monkeys import SeleniumClickerMonkey, SeleniumFormFillerMonkey
+from util.election_testing import remove_database_folder, remove_election_from_database, initialize_server, populate_credential_and_password_for_voters_from_sent_emails, populate_random_votes_for_voters
+from util.page_objects import BallotBoxPage
+from util.state_machine import ElectionHomePageState, NormalVoteStep6PageState
+from util.monkeys import SmartMonkeyWithMemoryAndKnownStateMachine
 from util.execution import console_log
 from test_scenario_2 import BeleniosTestElectionScenario2Base, initialize_browser
 import settings
-
-
-def verify_page_is_not_an_error_page(browser):
-    # Belenios web server returns a "Unauthorized" "Error 401" page in several situations, for example when we pick the "local" login method and submit an empty login form. For now, we consider this behaviour as normal.
-    # But what we consider an unexpected error is other types of errors returned by the server, for example "Internal Server Error", "Error 500".
-    error_content = ["Internal Server Error", "Error 500"]
-    page_source = browser.page_source
-    for content in error_content:
-        if content in page_source:
-            page_source = str(browser.page_source.encode("utf-8"))
-            raise Exception(f"Server returned an unexpected error page. Page source was: {page_source}")
 
 
 def get_election_url(election_id):
@@ -121,150 +112,45 @@ class BeleniosMonkeyTestClicker(BeleniosTestElectionScenario2Base):
         return target_fake_sent_emails_manager
 
 
-    @unittest.skip("Bug discovered using Monkey Testing: Logging in and clicking browser's Back button and logging in again produces an Internal Server Error")
-    def test_clicker_monkey_on_election_home(self):
-        console_log("# test_clicker_monkey_on_election_home()")
-        browser = self.browser
-        election_url = get_election_url(self.election_id)
-        console_log("## Going to election page:", election_url)
-
-        monkey = SeleniumClickerMonkey(browser, election_url, verify_page_is_not_an_error_page)
-        monkey.start(100)
-
-
-    @unittest.skip("TODO: remove skip")
-    def test_sometimes_smart_monkey_votes(self):
-        console_log("# test_sometimes_smart_monkey_votes()")
+    def test_very_smart_monkey_votes(self):
+        console_log("# test_very_smart_monkey_votes()")
         browser = self.browser
         timeout = settings.EXPLICIT_WAIT_TIMEOUT
         election_url = get_election_url(self.election_id)
         console_log("## Going to election page:", election_url)
         browser.get(election_url)
 
-        wait_a_bit()
+        election_home_page_state = ElectionHomePageState(browser, timeout)
+        election_home_page_state.page.click_on_language_link("en")
+        in_memory = {
+            "voter_username": settings.VOTER_USERNAME,
+            "voter_password": settings.VOTER_PASSWORD,
+            "voter_credential": settings.VOTER_CREDENTIAL,
+        }
+        smart_monkey = SmartMonkeyWithMemoryAndKnownStateMachine(election_home_page_state, in_memory=in_memory)
+        console_log(f"smart_monkey.current_state: {smart_monkey.current_state}")
+        console_log(f"smart_monkey.in_memory: {smart_monkey.in_memory}")
+        current_iteration = 1
+        while not isinstance(smart_monkey.current_state, NormalVoteStep6PageState):
+            smart_monkey.verify_page()
+            current_iteration += 1
+            console_log(f"executing action number {current_iteration}")
+            try:
+                smart_monkey.execute_a_random_action()
+            except Exception as e:
+                console_log("Exception while executing `smart_monkey.execute_a_random_action()`:", repr(e))
+                time.sleep(10)
+                raise Exception("Exception while executing `smart_monkey.execute_a_random_action()`") from e
+            console_log(f"smart_monkey.current_state: {smart_monkey.current_state}")
+            console_log(f"smart_monkey.in_memory: {smart_monkey.in_memory}")
 
-        console_log("## Starting monkey behaviour for a few clicks:")
-        monkey = SeleniumClickerMonkey(browser, election_url)
-        monkey.start(3)
-
-        console_log("## Going to election page again", election_url)
-        browser.get(election_url)
-
-        wait_a_bit()
-
-        console_log("## Clicking on 'en' language link")
-        election_home_page = ElectionHomePage(browser, timeout)
-        election_home_page.click_on_language_link("en")
-
-        wait_a_bit()
-
-        console_log("## Clicking on 'Start' button")
-        election_home_page.click_on_start_button()
-
-        wait_a_bit()
-
-        console_log("## Verifying that we are on step 1 page")
-        step_1_page = NormalVoteStep1Page(browser, timeout)
-        step_1_page.verify_page()
-
-        # Here:
-        # We cannot have a monkey behaviour, because there is only one button to click (the "here" button).
-        # We can go back. This goes back to election home page
-
-        console_log("## Clicking on 'here' button")
-        step_1_page.click_on_here_button_and_type_voter_credential(settings.VOTER_CREDENTIAL)
-
-        wait_a_bit()
-
-        step_2_page = NormalVoteStep2Page(browser, timeout)
-        step_2_page.verify_page()
-
-        # Here:
-        # We can check any checkbox for the question (check 0 to n checkboxes)
-        # We can click on the "Next" button
-        # We can go back. This would go back to the election home page (not to the "Step 1" page, which would probably have been the intuitive behaviour)
-
-        console_log("## Answering vote question by checking randomly some checkboxes")
-        step_2_parent_css_selector = "#question_div"
-        form_filler_monkey = SeleniumFormFillerMonkey(browser, step_2_parent_css_selector) # Warning: In the DOM of the vote page, step 2, checkboxes are not in a `<form>`.
-        form_filler_monkey.fill_form()
-
-        console_log("## Click on the 'Next' button")
-        step_2_page.click_on_next_button()
-
-        wait_a_bit()
-
-        console_log("## Verify that we are on step 3 and that page content is correct (ballot tracker is not empty)")
-        step_3_page = NormalVoteStep3Page(browser, timeout)
-        step_3_page.verify_page()
-        step_3_smart_ballot_tracker_value = step_3_page.get_smart_ballot_tracker_value()
-
-        # Here:
-        # We can click on the "Continue" button (`<input style="font-size:30px;" value="Continue" type="submit">`)
-        # We can click on the "Restart" button (`<button onclick="location.reload();">Restart</button>`). This goes back to step 1.
-
-        console_log("## Click on the 'Continue' button")
-        step_3_page.click_on_continue_button()
-
-        wait_a_bit()
-
-        # We arrive on the login form (if we have not already logged in during this visit, which could happen if we do a complex navigation after a first login. If we have already logged in, we arrive directly on the step 5 page)
-        console_log("## Verify that we are on login page")
-        login_page = VoterLoginPage(browser, timeout)
-        login_page.verify_page()
-
-        # Here:
-        # We can click on the "Login" button without filling the username nor password field
-        # We can click on the "Login" button after having filled the username and password fields with wrong data
-        # We can click on the "Login" button after having filled the username and password fields with correct data
-        # We can go back. This goes back to step 3.
-
-        # If we don't fill the form, or fill the form with wrong username/password, and click on the "Login" button, we arrive on an "Unauthorized" "Error 401" page.
-
-        # If we fill the form with correct data and click on the "Login" button, we arrive on step 5 page.
-
-        console_log("## Filling log in form and submitting it")
-        login_page.log_in(settings.VOTER_USERNAME, settings.VOTER_PASSWORD)
-
-        console_log("## Verify that we are on step 5 and that page content is correct (page contains 'has been received, but not recorded yet'; page contains a ballot tracker which is the same as the one we noted; page contains voter's username)")
-        step_5_page = NormalVoteStep5Page(browser, timeout)
-        step_5_page.verify_page(step_3_smart_ballot_tracker_value, settings.VOTER_USERNAME)
-
-        # Here:
-        # We can click on the Belenios logo on the top-left of the screen
-        # We can click on a link in the footer
-        # We can click on the "I cast my vote" button
-        # We can click on the "Go back to election" link
-        # We can go back. This goes back to the Login page which immediately redirects to this same step 5 page.
-
-        console_log("Click on the 'I cast my vote' button")
-        step_5_page.click_on_i_cast_my_vote_button()
-
-        wait_a_bit()
-
-        console_log("## Verify that we are on step 6 and that page content is correct (page contains 'has been accepted'; page contains a ballot tracker which is the same as the one we noted)")
-        step_6_page = NormalVoteStep6Page(browser, timeout)
-        step_6_page.verify_page(step_3_smart_ballot_tracker_value)
-
-        # Here:
-        # We can click on the Belenios logo on the top-left of the screen
-        # We can click on a link in the footer
-        # We can click on the "ballot box" link
-        # We can click on the "Go back to election" link
-        # We can go back. This goes to another page which looks like the "Advanced mode" page. This looks like a small bug.
-
-        console_log("## Click on the 'ballot box' link")
-        step_6_page.click_on_ballot_box_link()
-
-        wait_a_bit()
-
-        console_log("## Verify that ballot box page contains a link labelled as voter's smart ballot tracker, and click on it")
-        ballot_box_page = BallotBoxPage(browser, timeout)
-        ballot_box_page.verify_page(step_3_smart_ballot_tracker_value)
-        ballot_box_page.click_on_ballot_link(step_3_smart_ballot_tracker_value)
-
-        console_log("## Verify that my ballot page is not an error page")
-        verify_page_is_not_an_error_page(browser)
+            if isinstance(smart_monkey.current_state, NormalVoteStep6PageState):
+                console_log("Ending monkey behaviour here because we have completed the vote")
+                smart_monkey.verify_page()
+                console_log("Clicking on the ballot box link and verifying presence of voter's smart ballot tracker")
+                smart_monkey.current_state.page.click_on_ballot_box_link()
+                ballot_box_page = BallotBoxPage(browser, timeout)
+                ballot_box_page.verify_page(smart_monkey.get_memory_element("voter_validated_smart_ballot_tracker"))
 
 
 if __name__ == "__main__":
