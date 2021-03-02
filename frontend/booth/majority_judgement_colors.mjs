2b6a75e0c4594bd951f16eba45859a19a6d981d9
Swergas <swergas@yahoo.fr>
2021-03-02 01:04:55 +0100

    reverse order of colors to match backend index 0 = Excellent

--- frontend/booth/majority_judgement_colors.mjs
+++ frontend/booth/majority_judgement_colors.mjs
@@ -11,15 +11,15 @@ const gradeIndexToCssColor = (gradesHslColorScale, availableGradesLength, gradeI
 }; 
 
 /*
---majority-judgement-color-0: #b02800; / * Reject / À rejeter / hsl(14, 100%, 35%) * /
---majority-judgement-color-1: #ff6503; / * Poor / insuffisant / hsl(23, 100%, 51%) * /
---majority-judgement-color-2: #ff9f00; / * Acceptable / Passable / hsl(37, 100%, 50%) * /
---majority-judgement-color-3: #f5c823; / * Fair / Assez bien / hsl(47, 91%, 55%) * /
---majority-judgement-color-4: #7dd162; / * Good / Bien / hsl(105, 55%, 60%) * /
---majority-judgement-color-5: #2eb430; / * Very good / Très bien / hsl(121, 59%, 44%) * /
---majority-judgement-color-6: #0f7c10; / * Excellent / Excellent / hsl(121, 78%, 27%) * /
+#b02800: Reject / À rejeter / hsl(14, 100%, 35%)
+#ff6503: Poor / Insuffisant / hsl(23, 100%, 51%)
+#ff9f00: Acceptable / Passable / hsl(37, 100%, 50%)
+#f5c823: Fair / Assez bien / hsl(47, 91%, 55%)
+#7dd162: Good / Bien / hsl(105, 55%, 60%)
+#2eb430: Very good / Très bien / hsl(121, 59%, 44%)
+#0f7c10: Excellent / Excellent / hsl(121, 78%, 27%)
 */
-const majorityJudgementGradesHslColorScale = [[14, 100, 35], [23, 100, 51], [37, 100, 50], [47, 91, 55], [105, 55, 60], [121, 59, 44], [121, 78, 27]]; // from red to yellow to green
+const majorityJudgementGradesHslColorScale = [[121, 78, 27], [121, 59, 44], [105, 55, 60], [47, 91, 55], [37, 100, 50], [23, 100, 51], [14, 100, 35]]; // from green to yellow to red
 
 const majorityJudgementGradeIndexToCssColor = gradeIndexToCssColor.bind(null, majorityJudgementGradesHslColorScale);
 
