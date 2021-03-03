4bb4fa03357f1be80f009d2b2ce9a89c0f3631f6
Stephane Glondu <steph@glondu.net>
2021-03-03 10:22:16 +0100

    Add el to supported languages

--- src/web/web_common.ml
+++ src/web/web_common.ml
@@ -342,7 +342,11 @@ let split_identity_opt x =
      Some (int_of_string weight)
   | _ -> failwith "Web_common.split_identity_opt"
 
-let available_languages = ["en"; "fr"; "de"; "ro"; "it"; "nb"; "es"; "uk"; "cs"; "oc"; "pt_BR"]
+let available_languages = [
+    "en"; "fr"; "de"; "ro"; "it";
+    "nb"; "es"; "uk"; "cs"; "oc";
+    "pt_BR"; "el";
+  ]
 
 let get_languages xs =
   match xs with
