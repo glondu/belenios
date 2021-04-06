1331cb5062328873628cdf6191bd0285eb693f7b
Stephane Glondu <steph@glondu.net>
2021-04-06 13:16:20 +0200

    Add fi to available languages (for voter)

--- src/web/web_common.ml
+++ src/web/web_common.ml
@@ -350,7 +350,7 @@ let split_identity_opt x =
 let available_languages = [
     "en"; "fr"; "de"; "ro"; "it";
     "nb"; "es"; "uk"; "cs"; "oc";
-    "pt_BR"; "el"; "nl"; "sk";
+    "pt_BR"; "el"; "nl"; "sk"; "fi";
   ]
 
 let get_languages xs =
