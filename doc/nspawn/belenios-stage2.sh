74274769bb81724ff90dc008599ac759d7c5d8b5
Stephane Glondu <steph@glondu.net>
2021-07-09 13:34:39 +0200

    Do not use system opam in belenios-stage2.sh

--- doc/nspawn/belenios-stage2.sh
+++ doc/nspawn/belenios-stage2.sh
@@ -5,11 +5,11 @@ set -e
 echo "Installing Debian prerequisites..."
 
 apt-get update -qq && apt-get upgrade -qq
-apt-get install -qq build-essential libgmp-dev libpcre3-dev pkg-config m4 libssl-dev libsqlite3-dev wget ca-certificates zip unzip libncurses-dev zlib1g-dev libgd-securityimage-perl cracklib-runtime git jq npm rsync opam debootstrap squashfs-tools
+apt-get install -qq build-essential libgmp-dev libpcre3-dev pkg-config m4 libssl-dev libsqlite3-dev wget ca-certificates zip unzip libncurses-dev zlib1g-dev libgd-securityimage-perl cracklib-runtime git jq npm rsync debootstrap squashfs-tools
 apt-get clean
 
 echo "Running opam-bootstrap.sh..."
 
 useradd --create-home belenios
-su - belenios -c "env BELENIOS_USE_SYSTEM_OPAM=yes BELENIOS_OPAM_INIT_ARGS=--disable-sandboxing /opam-bootstrap.sh"
-su - belenios -c "head -n1 env.sh > opam-env.sh && . ./opam-env.sh && opam env >> opam-env.sh"
+su - belenios -c "env BELENIOS_OPAM_INIT_ARGS=--disable-sandboxing /opam-bootstrap.sh"
+su - belenios -c "head -n2 env.sh > opam-env.sh && . ./opam-env.sh && opam env >> opam-env.sh"
