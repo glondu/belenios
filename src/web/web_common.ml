63165dd317f5e8053611978fc862f916e1a20584
Vincent Laporte <Vincent.Laporte@inria.fr>
2021-07-15 10:20:46 +0200

    sendmail: populate the warn/error logs

--- src/web/web_common.ml
+++ src/web/web_common.ml
@@ -330,9 +330,12 @@ let send_email kind ~recipient ~subject ~body =
       (fun () -> Lwt_preemptive.detach sendmail contents)
       (function
        | Unix.Unix_error (Unix.EAGAIN, _, _) when retry > 0 ->
+          Ocsigen_messages.warning "Failed to fork for sending an e-mail; will try again in 1s";
           let* () = Lwt_unix.sleep 1. in
           loop (retry - 1)
-       | e -> Lwt.fail e
+       | e ->
+          Ocsigen_messages.errlog ("Failed to send an e-mail: " ^ Printexc.to_string e);
+          Lwt.fail e
       )
   in loop 2
 
