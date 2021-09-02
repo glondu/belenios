e1e2c07148b33f19cb7a3b637f7665c256aab54e
Stephane Glondu <steph@glondu.net>
2021-09-02 09:42:13 +0200

    Add "pl" to supported languages in new booth

--- frontend/booth/i18n_init.mjs
+++ frontend/booth/i18n_init.mjs
@@ -10,7 +10,7 @@ function i18n_init(language_code="en", onInitialized=null, onLanguageChanged=nul
       {
         lng: language_code,
         debug: true,
-        supportedLngs: ["en", "fr"],
+        supportedLngs: ["en", "fr", "pl"],
         fallbackLng: { // In order to not download the fallback language translation file when it won't be needed, languages in which the application is fully translated should have an empty array for their key here
           "fr": [],
           "en": [],
