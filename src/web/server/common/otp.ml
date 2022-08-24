86b48baf045bc68e589fe8a62fd32035999f7cc9
Stéphane Glondu <steph@glondu.net>
2022-08-24 13:34:12 +0200

    Limit the number of trials in OTPs

--- src/web/server/common/otp.ml
+++ src/web/server/common/otp.ml
@@ -39,25 +39,27 @@ module Make (I : SENDER) () = struct
     {
       code : string;
       expiration_time : datetime;
+      mutable trials_left : int;
     }
 
   let codes = ref SMap.empty
 
-  let filter_codes_by_time table =
-    let now = now () in
+  let filter_codes_by_time now table =
     SMap.filter (fun _ {expiration_time; _} ->
         datetime_compare now expiration_time <= 0
       ) table
 
   let generate ~address =
-    let codes_ = filter_codes_by_time !codes in
+    let now = now () in
+    let codes_ = filter_codes_by_time now !codes in
     let* code = generate_numeric () in
-    let expiration_time = datetime_add (now ()) (second 900) in
-    codes := SMap.add address {code; expiration_time} codes_;
+    let expiration_time = datetime_add now (second 900) in
+    codes := SMap.add address {code; expiration_time; trials_left = 10} codes_;
     I.send ~address ~code
 
   let check ~address ~code =
-    let codes_ = filter_codes_by_time !codes in
+    let now = now () in
+    let codes_ = filter_codes_by_time now !codes in
     codes := codes_;
     match SMap.find_opt address codes_ with
     | None -> false
@@ -65,6 +67,12 @@ module Make (I : SENDER) () = struct
        if x.code = code then (
          codes := SMap.remove address codes_;
          true
-       ) else false
+       ) else (
+         if x.trials_left > 0 then
+           x.trials_left <- x.trials_left - 1
+         else
+           codes := SMap.remove address codes_;
+         false
+       )
 
 end
