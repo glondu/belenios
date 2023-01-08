759e6247ebde6baeab788df412d7bc7311dc88fa
Stephane Glondu <steph@glondu.net>
2023-01-08 13:26:57 +0100

    New booth: Simplify NiceButton

--- frontend/booth/components/NiceButton.mjs
+++ frontend/booth/components/NiceButton.mjs
@@ -1,29 +1,13 @@
-const defaultDangerousLabel = {
-  __html: "hi"
-};
-
 // Parameter `tagName` can be "button", "input", "a"
-function NiceButton ({ label=null, dangerousLabel=null, onClick=null, disabled=false, styling="default", className=null, tagName="button", ...props }){
-  const attributes = {
-    className: className ? `nice-button nice-button--${styling} ${className}` : `nice-button nice-button--${styling}`,
-    onClick: onClick,
-    disabled: disabled
-  };
-  let bonusAttributes = {};
-  if (!label && dangerousLabel){
-    bonusAttributes['dangerouslySetInnerHTML'] = dangerousLabel;
-  }
+function NiceButton ({ label=null, styling="default", className=null, tagName="button", ...props }){
+  props["className"] = className ? `nice-button nice-button--${styling} ${className}` : `nice-button nice-button--${styling}`;
   if (tagName == "input"){
-    bonusAttributes['type'] = 'submit';
-    bonusAttributes['value'] = label;
+    props['type'] = 'submit';
+    props['value'] = label;
   }
   return e(
     tagName,
-    {
-      ...attributes,
-      ...bonusAttributes,
-      ...props
-    },
+    props,
     tagName == "input" ? null : label
   );
 }
