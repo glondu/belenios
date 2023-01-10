5c79029077ebeaf75ac76a8078c2a2f61e1201d7
Stéphane Glondu <steph@glondu.net>
2023-01-10 16:35:55 +0100

    Avoid a React warning about missing keys

--- frontend/booth/shortcuts.js
+++ frontend/booth/shortcuts.js
@@ -7,10 +7,10 @@ function txt_br(x) {
         const lines = x.split(/<br>/);
         var result = [];
         if (lines.length > 0) {
-            result.push(lines[0]);
+            result.push(e("span", {key: 0}, lines[0]));
             for (var i = 1; i < lines.length; i++) {
-                result.push(e("br"));
-                result.push(lines[i]);
+                result.push(e("br", {key: 2 * i - 1}));
+                result.push(e("span", {key: 2 * i}, lines[i]));
             }
         }
         return result;
