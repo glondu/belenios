aa1e9494768e265d417babacd5675283fc357f80
Stéphane Glondu <steph@glondu.net>
2023-01-13 13:05:06 +0100

    Single Webpack configuration

--- frontend/booth/webpack.config.js
+++ frontend/booth/webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './app.mjs',
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.development.js',
+        filename: 'bundle.js',
     },
 };
