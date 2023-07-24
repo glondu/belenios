89db14f385517e57dfefc5c5bdbea10ba2a4915e
Stephane Glondu <steph@glondu.net>
2023-07-24 10:41:57 +0200

    Use WebAssembly.instantiateStreaming

--- ext/libsodium/libsodium.js
+++ ext/libsodium/libsodium.js
@@ -29,28 +29,20 @@ function compute_base(uri) {
 const base = compute_base(location.href);
 
 if (typeof WebAssembly !== "undefined" && WebAssembly.instantiateStreaming) {
-    // TODO: use directly WebAssembly.instantiateStreaming(fetch("libsodium.wasm"), importObject).then(...)
-    // when ocaml-magic-mime supports wasm (https://github.com/mirage/ocaml-magic-mime/issues/27)
-    fetch(base + "/static/libsodium.wasm").then(function (response) {
-        if (response.ok) {
-            response.arrayBuffer().then(function (buffer) {
-                var env = {memset: memset};
-                var importObject = {env: env};
-                WebAssembly.instantiate(buffer, importObject).then(function (obj) {
-                    var raw = obj.instance.exports;
-                    var base = 0x10000 * raw.memory.grow(1);
-                    var buffer = new Uint8Array(raw.memory.buffer);
-                    belenios.libsodium = {
-                        bytes: raw.crypto_core_ed25519_bytes,
-                        scalarbytes: raw.crypto_core_ed25519_scalarbytes,
-                        is_valid_point: raw.crypto_core_ed25519_is_valid_point,
-                        scalarmult: raw.crypto_scalarmult_ed25519_noclamp,
-                        add: raw.crypto_core_ed25519_add,
-                        base: base,
-                        buffer: buffer
-                    };
-                });
-            });
-        }
+    var env = {memset: memset};
+    var importObject = {env: env};
+    WebAssembly.instantiateStreaming(fetch(base + "/static/libsodium.wasm"), importObject).then(function (obj) {
+        var raw = obj.instance.exports;
+        var base = 0x10000 * raw.memory.grow(1);
+        var buffer = new Uint8Array(raw.memory.buffer);
+        belenios.libsodium = {
+            bytes: raw.crypto_core_ed25519_bytes,
+            scalarbytes: raw.crypto_core_ed25519_scalarbytes,
+            is_valid_point: raw.crypto_core_ed25519_is_valid_point,
+            scalarmult: raw.crypto_scalarmult_ed25519_noclamp,
+            add: raw.crypto_core_ed25519_add,
+            base: base,
+            buffer: buffer
+        };
     });
 }
