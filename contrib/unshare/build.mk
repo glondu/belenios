f31a7026c64244567d6fd2e26e3c7d9d6f4c4d95
Stephane Glondu <steph@glondu.net>
2024-07-04 05:50:18 +0200

    Add a script to setup a build directory

---
+++ contrib/unshare/build.mk
@@ -0,0 +1,30 @@
+include Makefile.config
+
+CHANGES := belenios-opam_$(TOOLCHAIN_VERSION)_$(ARCH).changes
+ROOTFS := rootfs_$(TOOLCHAIN_VERSION)_$(ARCH).tar.zst
+VERSION := $(shell git -C "$(BELENIOS_SOURCES)" describe)
+SQUASHFS := belenios_$(VERSION)_$(ARCH).squashfs
+
+all: $(SQUASHFS)
+
+$(SQUASHFS): $(ROOTFS) $(CHANGES)
+	cd "$(BELENIOS_SOURCES)" && \
+	TMPDIR="$(BIGTMP)" \
+	contrib/unshare/make-squashfs.sh "$(SUITE)" \
+	  "$(CURDIR)/$(CHANGES)" "$(CURDIR)/$(ROOTFS)" \
+	  "$(CURDIR)/$(SQUASHFS)"
+
+$(ROOTFS): $(CHANGES)
+	cd "$(BELENIOS_SOURCES)" && \
+	TMPDIR="$(BIGTMP)" \
+	contrib/unshare/make-rootfs-dev.sh "$(SUITE)" \
+	  "$(CURDIR)/$(CHANGES)" "$(CURDIR)/$(ROOTFS)"
+
+$(CHANGES): stamp
+	cd "$(BELENIOS_SOURCES)" && \
+	TMPDIR="$(BIGTMP)" \
+	contrib/unshare/make-deb-belenios-opam.sh "$(SUITE)" \
+	  "" "$(TOOLCHAIN_VERSION)" "" "$(CURDIR)"
+
+stamp:
+	touch $@
