a717b3ebdc3bffd00328e6a22500c8b8614fc91d
Stephane Glondu <steph@glondu.net>
2025-02-19 09:16:52 +0100

    Group AES-CCM tests in Tool_js

--- src/web/clients/tool/tool_js.ml
+++ src/web/clients/tool/tool_js.ml
@@ -79,20 +79,23 @@ module Tests = struct
     let j = Z.of_bits "\x81\xab\xd3\xed\x0b\x19\x2e\x40\x7a\xca" in
     let k = Z.of_string "956173156978067279948673" in
     check "of_bits" (fun () -> Z.(j =% k));
-    let key =
-      "0000000000000000000000000000000000000000000000000000000000000000"
+    let* () =
+      let key =
+        "0000000000000000000000000000000000000000000000000000000000000000"
+      in
+      let iv = "00000000000000000000000000000000" in
+      let plaintext =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      in
+      let ciphertext =
+        "91f136cd65db6fa83b4943395e388089d4a8d0531b43a24a6498a1433559039ce5a18734752e13418718be1c2da5cca3d89e6e62fb729a81ec1cb3d1174e770c"
+      in
+      let* t_ciphertext = AES_CCM.encrypt ~key ~iv ~plaintext in
+      check "AES-CCM-encrypt" (fun () -> t_ciphertext = ciphertext);
+      let* t_plaintext = AES_CCM.decrypt ~key ~iv ~ciphertext in
+      check "AES-CCM-decrypt" (fun () -> t_plaintext = Some plaintext);
+      Lwt.return_unit
     in
-    let iv = "00000000000000000000000000000000" in
-    let plaintext =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    in
-    let ciphertext =
-      "91f136cd65db6fa83b4943395e388089d4a8d0531b43a24a6498a1433559039ce5a18734752e13418718be1c2da5cca3d89e6e62fb729a81ec1cb3d1174e770c"
-    in
-    let* t_ciphertext = AES_CCM.encrypt ~key ~iv ~plaintext in
-    check "AES-CCM-encrypt" (fun () -> t_ciphertext = ciphertext);
-    let* t_plaintext = AES_CCM.decrypt ~key ~iv ~ciphertext in
-    check "AES-CCM-decrypt" (fun () -> t_plaintext = Some plaintext);
     Printf.ksprintf alert "%d tests were successful!" !ntests;
     Lwt.return_unit
 
