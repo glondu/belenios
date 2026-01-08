9422f1811a00238ae194f8e3ca264cbf5fe9fa63
Stephane Glondu <steph@glondu.net>
2026-01-08 08:29:44 +0100

    Regenerate libsodium.wasm with up-to-date stack

--- vendor/libsodium/build.sh
+++ vendor/libsodium/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Prerequisites:
-# - Debian testing (2024-12-04)
+# - Debian testing (2026-01-08)
 # - wasm-ld (package lld)
 # - wasm-strip (package wabt)
 
@@ -12,17 +12,17 @@ echo "Using $TMP..."
 
 OLD="$PWD"
 cd $TMP
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.20-stable.tar.gz
-wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
-wget https://github.com/bytecodealliance/wasm-micro-runtime/releases/download/WAMR-2.2.0/iwasm-2.2.0-x86_64-ubuntu-22.04.tar.gz
+wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.21-stable.tar.gz
+wget https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz
+wget https://github.com/bytecodealliance/wasm-micro-runtime/releases/download/WAMR-2.4.4/iwasm-2.4.4-x86_64-ubuntu-22.04.tar.gz
 cat > SHA256SUM <<EOF
-86b2f81c0d88aeb7982051861129c28950071a2d3b7538b59782f8801c7364a0  iwasm-2.2.0-x86_64-ubuntu-22.04.tar.gz
-94fda0763264decb3b4f4bcd060588e81681616eecadfc00864c30f512ea1ada  libsodium-1.0.20-stable.tar.gz
-d45312e61ebcc48032b77bc4cf7fd6915c11fa16e4aad116b66c9468211230ea  zig-linux-x86_64-0.13.0.tar.xz
+ec60ff8daed26319dfc4371843c56ac2dfadd20e2218cbbca97aecb8b390b7a8  iwasm-2.4.4-x86_64-ubuntu-22.04.tar.gz
+168efc6991e5f133f3c749eee9cc121c49b19ec503127193fc8b5d5eda696188  libsodium-1.0.21-stable.tar.gz
+02aa270f183da276e5b5920b1dac44a63f1a49e55050ebde3aecc9eb82f93239  zig-x86_64-linux-0.15.2.tar.xz
 EOF
 sha256sum --check SHA256SUM
 for u in *.tar.*; do tar xf $u; done
-( cd libsodium-stable && PATH=$TMP:$TMP/zig-linux-x86_64-0.13.0:$PATH dist-build/wasm32-wasi.sh )
+( cd libsodium-stable && PATH=$TMP:$TMP/zig-x86_64-linux-0.15.2:$PATH dist-build/wasm32-wasi.sh )
 wasm-ld --no-entry --import-undefined \
   --export crypto_core_ed25519_is_valid_point \
   --export crypto_scalarmult_ed25519_noclamp \
